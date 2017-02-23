3ab0a7b5497f75b778769f9ab4edf186b41a3920
Sebe Keller <sebethkeller@gmail.com>
2017-02-22 20:17:35 -0500

    Fix typos

--- lib/one_time_pad.rb
+++ lib/one_time_pad.rb
@@ -6,7 +6,7 @@ class OneTimePad
 		key_length = msg.gsub(/[^a-zA-Z]/, "").length
 		key = ""
 		key_length.times do
-			key += (97 + rand(25)).chr
+			key += (97 + rand(26)).chr
 		end
 		cipher_text = @vigenere.encrypt(msg,key)
 		return [cipher_text,key]
